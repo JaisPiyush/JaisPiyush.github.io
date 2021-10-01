bc7a72905369968b11c7d2fe1fe2119743a2b391
Piyush Jaiswal <iampiyushjaiswal103@gmail.com>
2021-10-01 22:38:48 +0530

    showError added, some color themes added

--- index.js
+++ index.js
@@ -10,16 +10,31 @@ var commands = [
   "vi",
   "nano",
   "vim",
-  "clear"
+  "clear",
 ];
 
-function clearFrame(){}
+function clearFrame() {}
+
+function showInitialManPage(){
+  html = `
+  <p class="green-text"></p>
+  `;
+}
+
+
+function showError(command){
+  window.env.termOut.innerText = `terminal: ${command}: command not found ...`;
+}
 
 function executeCommand(key) {
-  if(key === "clear"){
-    clearFrame();
-    document.body.innerHTML = '';
-    addNewPrompt();
+  if (key === "clear") {
+    clearFrame(); 
+    document.body.innerHTML = "";
+    // addNewPrompt();
+  }else if(commands.includes(key)){
+
+  }else{
+    showError(key)
   }
 }
 
@@ -39,25 +54,24 @@ function resetEnv() {
   };
 }
 
+var text = "";
+
 function addNewPrompt() {
   resetEnv();
   if (window.env.container === null) {
     window.env.container = document.createElement("div");
-    console.log(window.env);
+    // console.log(window.env);
     window.env.container.setAttribute(
       "class",
       "term-container text-sm lg:text-xl flex flex-col"
     );
     window.env.container.innerHTML = `<div class="term-input flex flex-row"></div>
-                           <div class="term-help"></div>
-                           <div class="term-output"></div>
+                           <div class="term-help green-text"></div>
+                           <div class="term-output green-text"></div>
                            `;
     document.body.appendChild(window.env.container);
 
-    let elem = `<span class="green-text">[piyush@engineer </span>~<span class="green-text">]$</span>
-              <span class="active-input ml-2"></span>
-              <span class="blinker blink block w-2 h-4 lg:h-6 green-bg h-4"></span>
-             `;
+    let elem = `<span class="green-text"><span class="text-red-800">[</span><span class="purple-text">piyush</span>@<span class="purple-text">developer</span> </span><span class="green-text"><span class="text-red-800">]</span></span><span class="active-input ml-2"></span><span class="blinker blink block w-2 h-4 lg:h-6 green-bg h-4"></span>`.replace('\n','');
 
     window.env.terminalInput = document.querySelector(".term-input");
     window.env.terminalInput.innerHTML = elem;
@@ -65,27 +79,6 @@ function addNewPrompt() {
     window.env.blinker = document.querySelector(".blinker");
     window.env.termHelp = document.querySelector(".term-help");
     window.env.termOut = document.querySelector(".term-output");
-
-    let text = "";
-
-    window.addEventListener("keydown", function (e) {
-      // console.log(e);
-      if (e.key === "Backspace" || e.key === "Delete") {
-        text = text.slice(0, text.length - 1);
-      } else if (allowedText.includes(e.key.toLowerCase())) {
-        text += e.key;
-      }
-      window.env.activeInput.innerText = text;
-    });
-
-    window.addEventListener("keyup", function (e) {
-      if (e.key === "Enter") {
-        executeCommand(text);
-        text = "";
-        deHydratePrompt();
-        addNewPrompt();
-      }
-    });
   }
 }
 
@@ -96,10 +89,64 @@ function deHydratePrompt() {
   window.env.activeInput.classList.remove("active-input");
   window.env.terminalInput.classList.remove("term-input");
   window.env.container.classList.remove("term-container");
-//   resetEnv();
-  window.removeEventListener("keydown", function () {});
-  window.removeEventListener("keyup", function (e) {});
+  //   resetEnv();
+  // window.removeEventListener("keydown", function () {});
+  // window.removeEventListener("keyup", function (e) {});
   //   addNewPrompt()
 }
 
 addNewPrompt();
+
+var commandHistory = [];
+var commandIndex = null;
+
+function updateText(_text) {
+  if (_text !== undefined && _text !== null) {
+    text = _text;
+    window.env.activeInput.innerText = text;
+  }
+}
+
+function handleArrowKey(e) {
+  // console.log(commandIndex);
+  if (e.key === "ArrowUp" && commandIndex != null && commandIndex >= 0) {
+    updateText(commandHistory[commandIndex]);
+    commandIndex -= 1;
+  } else if (e.key === "ArrowDown" && commandIndex != null) {
+    if (commandIndex === commandHistory.length || commandIndex === -1) {
+      updateText("");
+      commandIndex = commandHistory.length;
+    } else if (commandIndex < commandHistory.length) {
+      updateText(commandHistory[commandIndex]);
+      commandIndex += 1;
+    }
+  }
+}
+
+window.addEventListener("keydown", function (e) {
+  // console.log(e);
+  if (e.key === "Backspace" || e.key === "Delete") {
+    text = text.slice(0, text.length - 1);
+  } else if (allowedText.includes(e.key.toLowerCase())) {
+    text += e.key;
+  } else if (e.key === "ArrowUp" || e.key === "ArrowDown") {
+    // console.log(e.key);
+    handleArrowKey(e);
+  }
+  window.env.activeInput.innerText = text;
+});
+
+window.addEventListener("keyup", function (e) {
+  if (e.key === "Enter") {
+    if (text.length > 0 && commandHistory[commandHistory.length - 1] !== text) {
+      commandHistory.push(text);
+    }
+    commandIndex = commandHistory.length - 1;
+    executeCommand(text);
+    text = "";
+    deHydratePrompt();
+    addNewPrompt();
+  }
+});
+
+
